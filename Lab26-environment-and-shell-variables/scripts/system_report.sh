f67f182f5ff2e355e75a9e3d5482a7663f8dd6ed
Abdul Rehman <abdul4rehman215@gmail.com>
2026-02-23 01:54:08 +0530

    Create system_report.sh

---
+++ Lab26-environment-and-shell-variables/scripts/system_report.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# System Report Script Using Variables and Command Substitution
+
+current_users=$(who | wc -l)
+disk_usage=$(df -h / | awk 'NR==2 {print $5}')
+
+echo "System Report:"
+echo "Active users: $current_users"
+echo "Root FS usage: $disk_usage"
